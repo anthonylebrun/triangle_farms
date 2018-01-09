a081dd89bb5cfda6f1c1af9566029f06358403da
Pierre Lebrun <anthonylebrun@gmail.com>
2018-01-09 15:20:05 -0500

    Some preliminary styling

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -47,6 +47,10 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
+    },
+    sass: {
+      // Enable SASS/SCSS support
+      mode: "native"
     }
   },
 
