f317d846472217977dfc76bb0ef6bb2eb72340a2
Lance Johnson <lancejjohnson@gmail.com>
2018-01-10 19:52:45 -0500

    Nitpicks on EventCache

--- lib/triangle_farms/calendar/event_cache.ex
+++ lib/triangle_farms/calendar/event_cache.ex
@@ -3,17 +3,27 @@ defmodule TriangleFarms.Calendar.EventCache do
   alias TriangleFarms.Calendar.EventAPI
 
   def by_month(month, year) do
-    ConCache.get_or_store(EventCache, "#{month}-#{year}", fn() ->
+    ConCache.get_or_store(EventCache, "#{month}-#{year}", fn ->
       EventAPI.new |> EventAPI.by_month(month, year)
     end)
   end
 
+  ## Removed unnecessary Enum.each
   def clear do
-    ConCache.ets(EventCache)
+    EventCache
+    |> ConCache.ets
     |> :ets.tab2list
-    |> Enum.map(fn ({key, _}) -> key end)
-    |> Enum.each(&delete/1)
+    |> Enum.map(fn {key, _} -> delete(key) end)
   end
+
+  ## Alternative - Extract the list enumeration to a function
+  # def clear do
+  #   EventCache |> :ets.tab2list |> delete_keys
+  # end
+
+  # def delete_keys(cache_values) do
+  #   for {key, _} <- cache_values, do: delete(key)
+  # end
 
   def delete(key) do
     ConCache.delete(EventCache, key)
