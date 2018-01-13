ea7291c4f5e1b4a19a3cd96f221e6bc755869f33
Pierre Lebrun <anthonylebrun@gmail.com>
2018-01-13 15:56:07 -0500

    Cleanup of EventCache

--- lib/triangle_farms/calendar/event_cache.ex
+++ lib/triangle_farms/calendar/event_cache.ex
@@ -8,24 +8,15 @@ defmodule TriangleFarms.Calendar.EventCache do
     end)
   end
 
-  ## Removed unnecessary Enum.each
   def clear do
-    EventCache
-    |> ConCache.ets
-    |> :ets.tab2list
-    |> Enum.map(fn {key, _} -> delete(key) end)
+    EventCache |> :ets.tab2list |> delete_keys
   end
-
-  ## Alternative - Extract the list enumeration to a function
-  # def clear do
-  #   EventCache |> :ets.tab2list |> delete_keys
-  # end
-
-  # def delete_keys(cache_values) do
-  #   for {key, _} <- cache_values, do: delete(key)
-  # end
 
   def delete(key) do
     ConCache.delete(EventCache, key)
+  end
+
+  defp delete_keys(cache_values) do
+    for {key, _} <- cache_values, do: delete(key)
   end
 end
