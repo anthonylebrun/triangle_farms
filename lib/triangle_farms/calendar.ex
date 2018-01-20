060c543268ad65f8527a3a412c0ad8438ee681e8
Pierre Lebrun <anthonylebrun@gmail.com>
2018-01-20 14:19:00 -0500

    Add context method to orchestrate it all

--- lib/triangle_farms/calendar.ex
+++ lib/triangle_farms/calendar.ex
@@ -1,2 +1,7 @@
 defmodule TriangleFarms.Calendar do
+  alias TriangleFarms.Calendar.EventAPI
+
+  def list_events_by_month(month, year) do
+    EventAPI.new |> EventAPI.by_month(month, year)
+  end
 end
