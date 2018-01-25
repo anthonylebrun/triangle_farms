07e51722f30526f460e0f50ea666c7d9ad712a33
Pierre Lebrun <anthonylebrun@gmail.com>
2018-01-25 16:14:20 -0500

    Do not start Ecto (we don't need it yet)

--- lib/triangle_farms/application.ex
+++ lib/triangle_farms/application.ex
@@ -15,7 +15,7 @@ defmodule TriangleFarms.Application do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(TriangleFarms.Repo, []),
+      # supervisor(TriangleFarms.Repo, []),
       # Start the endpoint when the application starts
       supervisor(TriangleFarmsWeb.Endpoint, []),
       # Start your own worker by calling: TriangleFarms.Worker.start_link(arg1, arg2, arg3)
